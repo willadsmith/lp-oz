bc6431efa0a016a9291367dcad58ef9dfd811651
Vladimir <vladimir@Vladimirs-MacBook-Pro.local>
2025-02-27 20:35:26 +0500

    add some buttons

--- src/content-blocks/contacts/contacts.jsx
+++ src/content-blocks/contacts/contacts.jsx
@@ -11,21 +11,32 @@ function Contacts(props) {
                     <span className='line'></span>
                     <>Контакты</>
                 </Space>
-                <Space className='contacts-block-content-buttons-block'>
+            </Space>
+            <Space className='contacts-block-content-buttons-block'>
+                <Space>
                     <Space className='contacts-block-content-buttons-block-1' vertical={true}>
-                        <img style={{ borderRadius: 4, marginBottom: 20, marginLeft: 30 }} alt='qr' src={QrVk} width='250px' />
+                        <img className='vk-qr-block' style={{ borderRadius: 4, marginBottom: 20, marginLeft: 30 }} alt='qr' src={QrVk} width='250px' />
                         <Button className='button-contacts-main'>
                             Отправить заявку
                         </Button>
                     </Space>
-                    <Space className='contacts-block-content-buttons-block-2' >
-                        <Button className='button-contacts-main'>Instagram</Button>
-                        <Button className='button-contacts-main'>Вконтакте</Button>
-                        <Button className='button-contacts-main'>Telegram</Button>
-                    </Space>
-                    <Space className='contacts-block-content-buttons-block-3'>
-                        <img className='person-photo-about-me' alt='about-me-photo' width={400} height={280} src={AboutMePhoto}/>
-                    </Space>
+                    <Flex vertical={true}>
+                        <Flex vertical={false}>
+                            <Space className='contacts-block-content-buttons-block-2' >
+                                <Button className='button-contacts-main'>Telegram</Button>
+                                <Button className='button-contacts-main'>Facebook</Button>
+                                <Button className='button-contacts-main'>Instagram</Button>
+                            </Space>
+                            <Space className='contacts-block-content-buttons-block-3'>
+                                <img className='person-photo-about-me' alt='about-me-photo' width={400} height={280} src={AboutMePhoto}/>
+                            </Space>
+                        </Flex>
+                        <Space>
+                            <Space className='contact-block-attentions'>
+                                * Facebook, Instagram принадлежит компании Meta Platforms Inc., которая признана экстремистской организацией
+                            </Space>
+                        </Space>
+                    </Flex>
                 </Space>
             </Space>
         </Flex>
